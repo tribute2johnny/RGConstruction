b1d773a1be2f68406460acee5cb87c564b0fb9d1
John MacLean <tribute2johnny@gmail.com>
2022-11-21 18:57:19 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+      <div className='head-left'>
+       Rg Construction Group Ltd
+      </div>
+      <div className='head-right'>
+        <ul>
+          <p>Phone: 01592 304037</p>
+          <p>Mobile: 07813 185620</p>
+          <p>Email: rg@rgconstructionco.co.uk</p>
+        </ul>
+      </div>
       </header>
+      <main className='App-main'>
+      main
+      </main>
+      <footer className='App-footer'>
+      foot
+      </footer>
     </div>
   );
 }
